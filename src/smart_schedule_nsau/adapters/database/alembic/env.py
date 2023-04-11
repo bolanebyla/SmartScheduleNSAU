3669ac347693b39b817cf3efeb807e6adf1a0a27
Алексей Лесков <alexleskov2000@gmail.com>
2023-04-11 10:52:37 +0700

    Включить автоопределение изменения типов полей таблиц

--- src/smart_schedule_nsau/adapters/database/alembic/env.py
+++ src/smart_schedule_nsau/adapters/database/alembic/env.py
@@ -43,6 +43,7 @@ def run_migrations_offline():
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        compare_type=True,
     )
 
     with context.begin_transaction():
@@ -50,7 +51,11 @@ def run_migrations_offline():
 
 
 def do_run_migrations(connection: Connection) -> None:
-    context.configure(connection=connection, target_metadata=target_metadata)
+    context.configure(
+        connection=connection,
+        target_metadata=target_metadata,
+        compare_type=True,
+    )
 
     with context.begin_transaction():
         context.run_migrations()
